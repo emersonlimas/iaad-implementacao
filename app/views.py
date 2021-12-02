ca51dc4e08a1ab3c185e8aa9e871e73490ac4742
Felipe Santana <felipecabocloo@gmail.com>
2021-12-01 23:18:46 -0300

    commit felipe
    commit com edit, update e delete

--- app/views.py
+++ app/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from app.forms import CLINICAForm, ESPECIALIDADEForm, MEDICOForm, PACIENTEForm, AGENDACONSULTAForm
 from app.models import CLINICA, ESPECIALIDADE, MEDICO, PACIENTE, AGENDACONSULTA
 
-#Read------------------------------------------------------------------------
+# Read------------------------------------------------------------------------
+
+
 def home(request):
     return render(request, 'index.html')
 
@@ -37,38 +39,38 @@ def tab_agenda(request):
     return render(request, 'tab_agenda.html', data)
 
 
-#FORM------------------------------------------------------------------------
-def form_clinica(request): 
+# FORM------------------------------------------------------------------------
+def form_clinica(request):
     data = {}
     data['form_clinica'] = CLINICAForm()
     return render(request, 'form_clinica.html', data)
 
 
-def form_espec(request): 
+def form_espec(request):
     data = {}
     data['form_espec'] = ESPECIALIDADEForm()
     return render(request, 'form_espec.html', data)
 
 
-def form_med(request): 
+def form_med(request):
     data = {}
     data['form_med'] = MEDICOForm()
-    return render(request, 'form_med.html', data) 
+    return render(request, 'form_med.html', data)
 
 
-def form_pac(request): 
+def form_pac(request):
     data = {}
     data['form_pac'] = PACIENTEForm()
-    return render(request, 'form_pac.html', data) 
+    return render(request, 'form_pac.html', data)
 
 
-def form_agenda(request): 
+def form_agenda(request):
     data = {}
     data['form_agenda'] = AGENDACONSULTAForm()
     return render(request, 'form_agenda.html', data)
 
 
-#CREATE------------------------------------------------------------------------
+# CREATE------------------------------------------------------------------------
 def create_clinica(request):
     form = CLINICAForm(request.POST or None)
     if form.is_valid():
@@ -87,19 +89,119 @@ def create_med(request):
     form = MEDICOForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('form_med')
+        return redirect('tab_med')
 
 
 def create_pac(request):
     form = PACIENTEForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('form_pac')
+        return redirect('tab_pac')
 
 
 def create_agenda(request):
     form = AGENDACONSULTAForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('form_agenda')
+        return redirect('tab_agenda')
 
+# EDIT------------------------------------------------------------------------
+
+
+def edit_clinica(request, pk):
+    data = {}
+    data['clinica'] = CLINICA.objects.get(pk=pk)
+    data['form_clinica'] = CLINICAForm(instance=data['clinica'])
+    return render(request, 'form_clinica.html', data)
+
+
+def edit_espec(request, pk):
+    data = {}
+    data['especialidade'] = ESPECIALIDADE.objects.get(pk=pk)
+    data['form_espec'] = ESPECIALIDADEForm(instance=data['especialidade'])
+    return render(request, 'form_espec.html', data)
+
+
+def edit_pac(request, pk):
+    data = {}
+    data['paciente'] = PACIENTE.objects.get(pk=pk)
+    data['form_pac'] = PACIENTEForm(instance=data['paciente'])
+    return render(request, 'form_pac.html', data)
+
+
+def edit_med(request, pk):
+    data = {}
+    data['medico'] = MEDICO.objects.get(pk=pk)
+    data['form_med'] = MEDICOForm(instance=data['medico'])
+    return render(request, 'form_med.html', data)
+
+# UPDATE------------------------------------------------------------------------
+
+
+def update_clinica(request, pk):
+    data = {}
+    data['clinica'] = CLINICA.objects.get(pk=pk)
+    form = CLINICAForm(request.POST or None, instance=data['clinica'])
+    if form.is_valid():
+        form.save()
+        return redirect('tab_clinica')
+
+
+def update_espec(request, pk):
+    data = {}
+    data['especialidade'] = ESPECIALIDADE.objects.get(pk=pk)
+    form = ESPECIALIDADEForm(request.POST or None,
+                             instance=data['especialidade'])
+    if form.is_valid():
+        form.save()
+        return redirect('tab_espec')
+
+
+def update_pac(request, pk):
+    data = {}
+    data['paciente'] = PACIENTE.objects.get(pk=pk)
+    form = PACIENTEForm(request.POST or None, instance=data['paciente'])
+    if form.is_valid():
+        form.save()
+        return redirect('tab_pac')
+
+
+def update_med(request, pk):
+    data = {}
+    data['medico'] = MEDICO.objects.get(pk=pk)
+    form = MEDICOForm(request.POST or None, instance=data['medico'])
+    if form.is_valid():
+        form.save()
+        return redirect('tab_med')
+
+# DELETE--------------------------------------------------------------------------
+
+
+def delete_clinica(request, pk):
+    clinica = CLINICA.objects.get(pk=pk)
+    clinica.delete()
+    return redirect('tab_clinica')
+
+
+def delete_espec(request, pk):
+    espec = ESPECIALIDADE.objects.get(pk=pk)
+    espec.delete()
+    return redirect('tab_espec')
+
+
+def delete_med(request, pk):
+    med = MEDICO.objects.get(pk=pk)
+    med.delete()
+    return redirect('tab_med')
+
+
+def delete_pac(request, pk):
+    pac = PACIENTE.objects.get(pk=pk)
+    pac.delete()
+    return redirect('tab_pac')
+
+
+def delete_agenda(request, pk):
+    agenda = AGENDACONSULTA.objects.get(pk=pk)
+    agenda.delete()
+    return redirect('tab_agenda')
--- projeto/urls.py
+++ projeto/urls.py
@@ -18,28 +18,46 @@ from django.urls import path
 from app.views import home, tab_clinica, tab_espec, tab_med, tab_pac, tab_agenda
 from app.views import form_clinica, form_espec, form_med, form_pac, form_agenda
 from app.views import create_clinica, create_espec, create_med, create_pac, create_agenda
+from app.views import edit_clinica, edit_espec, edit_pac, edit_med
+from app.views import update_clinica, update_espec, update_pac, update_med
+from app.views import delete_clinica, delete_espec, delete_med, delete_pac, delete_agenda
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #READ------------------------------------------------------------------------
+    # READ------------------------------------------------------------------------
     path('', home, name='home'),
     path('tab_clinica/', tab_clinica, name='tab_clinica'),
     path('tab_espec/', tab_espec, name='tab_espec'),
     path('tab_med/', tab_med, name='tab_med'),
     path('tab_pac/', tab_pac, name='tab_pac'),
     path('tab_agenda/', tab_agenda, name='tab_agenda'),
-    #FORM------------------------------------------------------------------------
-    path('form_clinica/', form_clinica, name='form_clinica'), 
+    # FORM------------------------------------------------------------------------
+    path('form_clinica/', form_clinica, name='form_clinica'),
     path('form_espec/', form_espec, name='form_espec'),
     path('form_med/', form_med, name='form_med'),
     path('form_pac/', form_pac, name='form_pac'),
     path('form_agenda/', form_agenda, name='form_agenda'),
-    #CREATE------------------------------------------------------------------------
-    path('create_clinica', create_clinica, name='create_clinica'), 
+    # CREATE------------------------------------------------------------------------
+    path('create_clinica', create_clinica, name='create_clinica'),
     path('create_espec', create_espec, name='create_espec'),
     path('create_med', create_med, name='create_med'),
     path('create_pac', create_pac, name='create_pac'),
     path('create_agenda', create_agenda, name='create_agenda'),
+    # EDIT------------------------------------------------------------------------
+    path('edit_clinica/<int:pk>/', edit_clinica, name='edit_clinica'),
+    path('edit_espec/<int:pk>/', edit_espec, name='edit_espec'),
+    path('edit_pac/<int:pk>/', edit_pac, name='edit_pac'),
+    path('edit_med/<int:pk>/', edit_med, name='edit_med'),
+    # UPDATE------------------------------------------------------------------------
+    path('update_clinica/<int:pk>/', update_clinica, name='update_clinica'),
+    path('update_espec/<int:pk>/', update_espec, name='update_espec'),
+    path('update_pac/<int:pk>/', update_pac, name='update_pac'),
+    path('update_med/<int:pk>/', update_med, name='update_med'),
+    # DELETE-------------------------------------------------------------------------
+    path('delete_clinica/<int:pk>/', delete_clinica, name='delete_clinica'),
+    path('delete_espec/<int:pk>/', delete_espec, name='delete_espec'),
+    path('delete_med/<int:pk>/', delete_med, name='delete_med'),
+    path('delete_pac/<int:pk>/', delete_pac, name='delete_pac'),
+    path('delete_agenda/<int:pk>/', delete_agenda, name='delete_agenda'),
 ]
-
